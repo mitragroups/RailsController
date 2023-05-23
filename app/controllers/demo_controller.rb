414672626f70225705e7f42e81959704e244ffb6
mitragroups <miftahmuhajir@gmail.com>
2023-05-23 08:34:34 +0000

    Initial Develop

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,16 @@
 class DemoController < ApplicationController
   def index
   end
+
+  def coba
+    render 'home/index'
+  end
+
+  def iseng
+    render plain: 'Iseng - iseng aja'
+  end
+
+  def oke
+    redirect_to controller: 'home', action: 'index'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'demo/index'
+  get 'demo/coba'
+  get 'demo/iseng'
+  get 'demo/oke'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
